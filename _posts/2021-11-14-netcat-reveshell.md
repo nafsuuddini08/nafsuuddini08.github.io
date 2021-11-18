ab1cec0625ee0cb94263f43d1aba66060535b757
nafsuuddini08 <unafsu@gmail.com>
2021-11-18 03:04:16 +0100

    update 2 netcat post

--- _posts/2021-11-14-netcat-reveshell.md
+++ _posts/2021-11-14-netcat-reveshell.md
@@ -20,4 +20,45 @@ tags:
 <p align = "center">
 <img src = "/assets/images/img-netcat/netcat.jpg">
 </p>
-	
+
+We are going to learn how we can reverse shell in every OS, and some netcat commands that can help us when we are pentesting or scanning our environment.
+
+We are going to learn:
+
++ What is netcat
++ Basics netcat commands
++ Web server in netcat
++ What is reverse shell
++ Create a reverse shell in netcat
++ Reverse shell scripts
+
+## What is netcat?
+
+Netcat ***(The Network Swiss Army knife)*** it is a command line tool that reads and writes data over network connections using TCP, allows us to open TCP and UDP ports for listening and allows us to scan ports similar to nmap but with some limitations. The main use of this tool is in reverse shell.
+
+## Basics netcat commands
+
+On linux systems netcat is installed by default and if it is not installed we use the following command:
+
+```
+sudo apt install netcat
+```
+In some Windows versions it is not installed by default, so we have to install the file called nc.exe.
+
+With the ***-help*** parameter we can see the possibilities that netcat offers us and if we want to know more information about each parameter is to use the command ***man*** to see the netcat documentation. 
+
+<p align = "center">
+<img src = "/assets/images/img-netcat/captura1.png">
+</p>
+
+Scan ports. we can specify port ranges to report if any of those port ranges are open. it doesn't do a full scan like nmap, but it reports back to the console if the port we specified is open or not.
+
+<p align = "center">
+<img src = "/assets/images/img-netcat/captura2.png">
+</p>
+
+With the ***-u*** parameter we specify that we want to scan udp ports.
+
+<p align = "center">
+<img src = "/assets/images/img-netcat/captura3.png">
+</p>
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "Configure dhcp server in linux with isc-dhcp-server",
         "excerpt":"we are going to learn how to configure a dhcp server in linux using the isc-dhcp-server service, we are going to learn: create a fixed ip for our server machine, create subnets, exclude ips ranges, create an ips address concession and finally make a reserved ip for a specific host...","categories": ["Networking","Subnetting"],
         "tags": ["dhcp","isc-dhcp-server","Linux"],
-        "url": "http://192.168.43.131:4000/isc-dhcp-server/",
-        "teaser":"http://192.168.43.131:4000/assets/images/img-dhcp/servidor-DHCP-e1511228735448.png"},{
+        "url": "http://192.168.118.128:4000/isc-dhcp-server/",
+        "teaser":"http://192.168.118.128:4000/assets/images/img-dhcp/servidor-DHCP-e1511228735448.png"},{
         "title": "Azure - Appservice",
         "excerpt":"We are going to learn how the app service works and the possibilities it offers us to manage the hosting since the app service is still a hosting but the difference is that we can manage it. In this article we are going to learn: What is app service and...","categories": ["Cloud","web"],
         "tags": ["Azure","App service","git","github","Azure devops","Docker","webhook"],
-        "url": "http://192.168.43.131:4000/azure-appservice/",
-        "teaser":"http://192.168.43.131:4000/assets/images/img-appservice/appservice.png"},{
+        "url": "http://192.168.118.128:4000/azure-appservice/",
+        "teaser":"http://192.168.118.128:4000/assets/images/img-appservice/appservice.png"},{
         "title": "Scanning network with nmap",
         "excerpt":"We are going to learn how to scan a network or a host using nmap, we are going to see some simple nmap commands and commands that help us to pentest, and we are going to use some nmap scripts to speed up the scanning process. We are going to...","categories": ["Networking","Pentest"],
         "tags": ["Nmap","Linux","Hack the box"],
-        "url": "http://192.168.43.131:4000/nmap-guide/",
-        "teaser":"http://192.168.43.131:4000/assets/images/img-nmap/nmap.jpg"},{
+        "url": "http://192.168.118.128:4000/nmap-guide/",
+        "teaser":"http://192.168.118.128:4000/assets/images/img-nmap/nmap.jpg"},{
         "title": "Reverse shell using netcat",
-        "excerpt":"     ","categories": ["Networking","Pentest"],
+        "excerpt":"We are going to learn how we can reverse shell in every OS, and some netcat commands that can help us when we are pentesting or scanning our environment. We are going to learn: What is netcat Basics netcat commands Web server in netcat What is reverse shell Create a...","categories": ["Networking","Pentest"],
         "tags": ["netcat","Linux","reverse shell"],
-        "url": "http://192.168.43.131:4000/netcat-reveshell/",
-        "teaser":"http://192.168.43.131:4000/assets/images/img-netcat/netcat.jpg"}]
+        "url": "http://192.168.118.128:4000/netcat-reveshell/",
+        "teaser":"http://192.168.118.128:4000/assets/images/img-netcat/netcat.jpg"}]
